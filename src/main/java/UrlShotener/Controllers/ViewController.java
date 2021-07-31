d67e1bf567c69e880566f8ccacbcd3cbb0920a15
Олег Завьялов <O.romanowi4@gmail.com>
2021-07-31 14:04:00 +0700

    добавлены view

--- src/main/java/UrlShotener/Controllers/ViewController.java
+++ src/main/java/UrlShotener/Controllers/ViewController.java
@@ -1,13 +1,28 @@
 package UrlShotener.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ViewController {
     @GetMapping("/")
-    public String index()
+    public String index(Model model)
     {
-        return "Hello";
+        model.addAttribute("module", "index");
+        return "index";
+    }
+    @GetMapping("/links")
+    public String links(Model model)
+    {
+        model.addAttribute("module", "links");
+        return "links";
+    }
+    @GetMapping("/about")
+    public String about(Model model)
+    {
+        model.addAttribute("module", "about");
+        return "about";
     }
 }
